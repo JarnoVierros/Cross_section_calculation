98e144eaa0946cef4c3e9caba5d400ae17a87f2e
JarnoVierros <vierros.jarno@gmail.com>
2024-06-20 15:56:07 +0200

    Implemented transverse polarization integration

--- int_test_1.cpp
+++ int_test_1.cpp
@@ -42,12 +42,12 @@ double T_integrand(double r_x, double r_y, double z) {
   return (m*m*gsl_pow_2(gsl_sf_bessel_K0(epsilon(z)*r)) + gsl_pow_2(epsilon(z))*(z*z + gsl_pow_2(1-z))*gsl_pow_2(gsl_sf_bessel_K1(epsilon(z)*r)))*dipole_amplitude(r);
 }
 
-double g(double *k, size_t dim, void *params) {
+double L_g(double *k, size_t dim, void *params) {
   return normalization*L_integrand(k[0], k[1], k[2]);
 }
 
-double f(double *k, size_t dim, void *params) {
-  return normalization*L_integrand(k[0], k[1], k[2]);
+double T_g(double *k, size_t dim, void *params) {
+  return normalization*T_integrand(k[0], k[1], k[2]);
 }
 
 int main() {
@@ -59,12 +59,14 @@ int main() {
   double xu[3] = {100, 100, 1};
 
   cout << "normalization: " << normalization << endl;
-  cout << "L_integrand: " << L_integrand(-100, -100, 0.5) << endl;
+  cout << "extreme L_integrand: " << L_integrand(-100, -100, 0.5) << endl;
+  cout << "extreme T_integrand: " << T_integrand(-100, -100, 0.5) << endl;
 
   const gsl_rng_type *T;
   gsl_rng *rng;
 
-  gsl_monte_function G = {&g, dim, 0};
+  gsl_monte_function L_G = {&L_g, dim, 0};
+  gsl_monte_function T_G = {&T_g, dim, 0};
 
   gsl_rng_env_setup ();
 
@@ -72,37 +74,46 @@ int main() {
   rng = gsl_rng_alloc(T);
 
 
-  gsl_monte_vegas_state *s = gsl_monte_vegas_alloc(dim);
+  gsl_monte_vegas_state *L_s = gsl_monte_vegas_alloc(dim);
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 10000, rng, s, &res, &err);
+  gsl_monte_vegas_integrate (&L_G, xl, xu, dim, 10000, rng, L_s, &res, &err);
 
+  cout << "L warmup" << endl;
   cout << "res: " << res << endl;
   cout << "err: " << err << endl;
   cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
   cout << endl;
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, rng, s, &res, &err);
+  for (int i=0; i<3; i++) {
+    gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, rng, L_s, &res, &err);
 
+    cout << "res: " << res << endl;
+    cout << "err: " << err << endl;
+    cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
+    cout << endl;
+  }
+
+  gsl_monte_vegas_free(T_s);
+
+  gsl_monte_vegas_state *T_s = gsl_monte_vegas_alloc(dim);
+
+  gsl_monte_vegas_integrate (&T_G, xl, xu, dim, 10000, rng, T_s, &res, &err);
+
+  cout << "T warmup" << endl;
   cout << "res: " << res << endl;
   cout << "err: " << err << endl;
-  cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
+  cout << "chisq: " << gsl_monte_vegas_chisq(T_s) << endl;
   cout << endl;
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, rng, s, &res, &err);
+  for (int i=0; i<3; i++) {
+    gsl_monte_vegas_integrate (&T_G, xl, xu, dim, 100000, rng, T_s, &res, &err);
 
-  cout << "res: " << res << endl;
-  cout << "err: " << err << endl;
-  cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
-  cout << endl;
+    cout << "res: " << res << endl;
+    cout << "err: " << err << endl;
+    cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
+    cout << endl;
+  }
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, rng, s, &res, &err);
-
-  cout << "res: " << res << endl;
-  cout << "err: " << err << endl;
-  cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
-
-
-  gsl_monte_vegas_free(s);
   gsl_rng_free(rng);
 
   return 0;
