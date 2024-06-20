50a7f8416f62e858cda9ddc7e1fb97687e9f80a6
JarnoVierros <vierros.jarno@gmail.com>
2024-06-20 15:09:15 +0200

    Fix Bessel function and write down transverse integrand

--- int_test_1.cpp
+++ int_test_1.cpp
@@ -22,7 +22,7 @@ const double lambda_star = 0.288;
 double Q = 1;
 double x = 1;
 
-double normalization = 4*alpha_em*N_c*e_f*e_f/(2*M_PI*2*M_PI)*4*Q*Q;
+double normalization = 4*alpha_em*N_c*e_f*e_f/(2*M_PI*2*M_PI);
 
 double epsilon(double z) {
   return sqrt(m*m + z*(1-z)*Q*Q);
@@ -34,10 +34,19 @@ double dipole_amplitude(double r) {
 
 double L_integrand(double r_x, double r_y, double z) {
   double r = sqrt(r_x*r_x + r_y*r_y);
-  return z*z*(1-z)*(1-z)*gsl_pow_2(gsl_sf_bessel_K1(epsilon(z)*r))*dipole_amplitude(r);
+  return 4*Q*Q*z*z*(1-z)*(1-z)*gsl_pow_2(gsl_sf_bessel_K0(epsilon(z)*r))*dipole_amplitude(r);
+}
+
+double T_integrand(double r_x, double r_y, double z) {
+  double r = sqrt(r_x*r_x + r_y*r_y);
+  return (m*m*gsl_pow_2(gsl_sf_bessel_K0(esilon(z)*r)) + gsl_pow_2(epsilon(z))*(z*z + gsl_pow_2(1-z))*gsl_pow_2(gsl_sf_bessel_K1(esilon(z)*r)))*dipole_amplitude(r);
 }
 
 double g(double *k, size_t dim, void *params) {
+  return normalization*L_integrand(k[0], k[1], k[2]);
+}
+
+double f(double *k, size_t dim, void *params) {
   return normalization*L_integrand(k[0], k[1], k[2]);
 }
 
@@ -53,34 +62,34 @@ int main() {
   cout << "L_integrand: " << L_integrand(-100, -100, 0.5) << endl;
 
   const gsl_rng_type *T;
-  gsl_rng *r;
+  gsl_rng *rng;
 
   gsl_monte_function G = {&g, dim, 0};
 
   gsl_rng_env_setup ();
 
   T = gsl_rng_default;
-  r = gsl_rng_alloc (T);
+  rng = gsl_rng_alloc(T);
 
 
   gsl_monte_vegas_state *s = gsl_monte_vegas_alloc(dim);
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 10000, r, s, &res, &err);
+  gsl_monte_vegas_integrate (&G, xl, xu, dim, 10000, rng, s, &res, &err);
 
   cout << "res: " << res << endl;
   cout << "err: " << err << endl;
   cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
   cout << endl;
 
-  gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, r, s, &res, &err);
+  gsl_monte_vegas_integrate (&G, xl, xu, dim, 100000, rng, s, &res, &err);
 
   cout << "res: " << res << endl;
   cout << "err: " << err << endl;
   cout << "chisq: " << gsl_monte_vegas_chisq(s) << endl;
 
 
-  gsl_monte_vegas_free (s);
-  gsl_rng_free (r);
+  gsl_monte_vegas_free(s);
+  gsl_rng_free(rng);
 
   return 0;
 }
