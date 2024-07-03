9f3fd5b882bfa7e1065dccc5c0dae06d58d19237
JarnoVierros <vierros.jarno@gmail.com>
2024-07-03 12:42:30 +0200

    Switched to new fit parameters

--- inclusive_ccbar_cross_section.cpp
+++ inclusive_ccbar_cross_section.cpp
@@ -20,10 +20,10 @@ const int N_c = 3;
 const double e_f = 2.0/3;
 const double m_f = 1.27; //GeV
 
-const double sigma_0 = 29.12; //mb
+const double sigma_0 = 2.99416e+01; //mb
 const double Q_0 = 1; //GeV
-const double x_0 = 0.000041;
-const double lambda_star = 0.288;
+const double x_0 = 7.67079e-05;
+const double lambda_star = 3.64361e-01;
 
 const double normalization = 4*alpha_em*N_c*e_f*e_f/(2*M_PI*2*M_PI);
 
