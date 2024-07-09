d31fd85172f5796539c3530748013ca501de0ebc
JarnoVierros <vierros.jarno@gmail.com>
2024-07-09 11:21:57 +0200

    New test

--- test.cpp
+++ test.cpp
@@ -7,18 +7,17 @@ int main() {
     static array<array<array<array<double, 4>, 81>, 900>, 30> table;
     string filename = "data/dipole_amplitude_with_IP_dependence.csv";
     
-    cout << "test 1" << endl;
     load_dipole_amplitudes(table, filename);
-    cout << "test 2" << endl;
-    /*
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<5; j++) {
-            for (int k=0; k<5; k++) {
+    
+    for (int i=0; i<30; i++) {
+        for (int j=0; j<900; j++) {
+            for (int k=0; k<81; k++) {
                 cout << table[i][j][k][0] << ", " << table[i][j][k][1] << ", " << table[i][j][k][2] << ", " << table[i][j][k][3] << endl;
+                cin;
             }
         }
     }
-    */
+    /*
     auto start = chrono::high_resolution_clock::now();
     double res = get_dipole_amplitude(table, 121.77, 11.84, 1e-05);
     auto stop = chrono::high_resolution_clock::now();
@@ -27,5 +26,6 @@ int main() {
     cout << "duration: " << duration.count() << endl;
 
     cout << "amplitude: " << res << endl;
+    */
     //0.000155246, 2.43256e-05, 1.12535e-09, 4.95126e-06
 }
