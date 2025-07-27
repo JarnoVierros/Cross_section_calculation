37ad82678bc005720c4748f391bcc2038ad6aae6
JarnoVierros <vierros.jarno@gmail.com>
2025-07-27 23:51:24 +0200

    some changes

--- qqg_contribution_calculator.cpp
+++ qqg_contribution_calculator.cpp
@@ -31,14 +31,14 @@ const double alpha_s = 0.25;
 static double e_f = 2.0/3;
 static double m_f = 1.27;
 
-static double r_limit = 100; // 100
+//static double r_limit = 100; // 100
 //static double b_min_limit; // 17.32
 
 const bool print_r_limit = false;
 const bool print_b_min_limit = false;
 
-const int warmup_calls = 1000;
-const int integration_calls = 10000;//20 000 000
+const int warmup_calls = 100000;
+const int integration_calls = 1000000;//100000000
 const int integration_iterations = 1;
 
 const string dipole_amp_type = "bk";
@@ -61,7 +61,7 @@ const double N_0 = 0.7;
 const double gamma_c = 0.7376;
 const double kappa = 9.9;
 const double const_alpha = N_0*gamma_c/(8*(1-N_0));
-const double const_beta = 1/2*exp(-(1-N_0)/(N_0*gamma_c)*log(1-N_0));
+const double const_beta = 1.0/2*exp(-(1-N_0)/(N_0*gamma_c)*log(1-N_0));
 
 
 //static array<array<array<array<array<double, 5>, 81>, 30>, 30>, 30> p_table;
@@ -135,6 +135,11 @@ double Q_s(double x) {
 
 double no_b_dipamp(double rQ_s, double x) {
 
+//cout << "rQ_s: " << rQ_s << endl;
+//cout << "x: " << x << endl;
+//cout << "log arg: " << const_beta*rQ_s << endl;
+//cout << "const_beta: " << const_beta << endl;
+
 if (rQ_s <= 2) {
     return N_0*pow(rQ_s/2, 2*gamma_c)*exp(-1*(2*pow(log(rQ_s/2), 2))/(kappa*lambda*log(1/x)));
 } else {
@@ -165,11 +170,11 @@ double Ig_integrand(double r, void * parameters) {
   double c = Q_s(x)/sqrt(k2);
   double value = r*gsl_sf_bessel_Jn(2, a*r)*gsl_sf_bessel_Kn(2, b*r)*no_b_dipamp(c*r, xpom);
 
-  cout << gsl_sf_bessel_Jn(2, a*r) << endl;
-  cout << gsl_sf_bessel_Kn(2, b*r) << endl;
-  cout << no_b_dipamp(c*r, x) << endl;
-  cout << "k2=" << k2 << endl;
-  cout << "beta=" << beta << ", z=" << z << ", r=" << r << ", rQs=" << c*r << ", x=" << x <<  ", value=" << value << endl << endl;
+  //cout << gsl_sf_bessel_Jn(2, a*r) << endl;
+  //cout << gsl_sf_bessel_Kn(2, b*r) << endl;
+  //cout << no_b_dipamp(c*r, x) << endl;
+  //cout << "k2=" << k2 << endl;
+  //cout << "beta=" << beta << ", z=" << z << ", r=" << r << ", rQs=" << c*r << ", x=" << x <<  ", value=" << value << endl << endl;
   return value;
 }
 
@@ -243,13 +248,10 @@ double xpomFqqg_LLQ2(double beta, double xpom, double Q2, double &result, double
   gsl_rng_set(rng, 1);
 
   gsl_monte_vegas_state *T_s = gsl_monte_vegas_alloc(dim);
-  cout << "test 1" << endl;
   status = gsl_monte_vegas_integrate(&qqg_LLQ2_rng_G, xl, xu, dim, warmup_calls, rng, T_s, &res, &err);
   if (status != 0) {cout << "qqg_LLQ2_warmup_error: " << status << endl; throw (status);}
-  cout << "test 2" << endl;
   status = gsl_monte_vegas_integrate(&qqg_LLQ2_rng_G, xl, xu, dim, integration_calls, rng, T_s, &res, &err);
   if (status != 0) {cout << "qqg_LLQ2_integration_error: " << status << endl; throw (status);}
-  cout << "test 3" << endl;
 
   if (gsl_isnan(res)) {
     res = 0;
@@ -265,7 +267,7 @@ double xpomFqqg_LLQ2(double beta, double xpom, double Q2, double &result, double
   return 0;
 }
 
-double Fqqg_LLbeta_integrand(double beta, double xpom, double Q2, double r, double z, double Rx, double Ry) {
+double cartesian_Fqqg_LLbeta_integrand(double beta, double xpom, double Q2, double r, double z, double Rx, double Ry) {
   double normalization = C_f*alpha_s*Q2*sigma_0/(8*gsl_pow_3(M_PI)*alpha_em);
 
   double epsilon = sqrt(z*(1-z)*Q2 + m_f*m_f);
@@ -276,7 +278,26 @@ double Fqqg_LLbeta_integrand(double beta, double xpom, double Q2, double r, doub
   double R = sqrt(Rx*Rx + Ry*Ry);
   double rminusR = sqrt(gsl_pow_2(r-Rx) + gsl_pow_2(0-Ry));
   double A = r*r/(R*R*gsl_pow_2(rminusR))*gsl_pow_2(no_b_dipamp(R*Qs, xpom) + no_b_dipamp(rminusR*Qs, xpom) - no_b_dipamp(r*Qs, xpom) - no_b_dipamp(R*Qs, xpom)*no_b_dipamp(rminusR*Qs, xpom));
+  cout << normalization*r*Phi_T*A << ", " << A << endl;
+  return normalization*r*Phi_T*A;
+}
 
+double polar_Fqqg_LLbeta_integrand(double beta, double xpom, double Q2, double r, double z, double R, double theta) {
+  double normalization = C_f*alpha_s*Q2*sigma_0/(8*gsl_pow_3(M_PI)*alpha_em);
+
+  double epsilon = sqrt(z*(1-z)*Q2 + m_f*m_f);
+  double Phi_T = alpha_em*N_c/(2*M_PI*M_PI)*e_f*e_f*((z*z+gsl_pow_2(1-z))*epsilon*epsilon*gsl_pow_2(gsl_sf_bessel_K1(epsilon*r)) + m_f*m_f*gsl_pow_2(gsl_sf_bessel_K0(epsilon*r)));
+
+  double x = beta*xpom;
+  double Qs = Q_s(x);
+  double rminusR = sqrt(r*r - 2*r*R*cos(theta) + R*R);
+  double A = 2*r*r/(R*R*gsl_pow_2(rminusR))*gsl_pow_2(no_b_dipamp(R*Qs, xpom) + no_b_dipamp(rminusR*Qs, xpom) - no_b_dipamp(r*Qs, xpom) - no_b_dipamp(R*Qs, xpom)*no_b_dipamp(rminusR*Qs, xpom));
+  if (A>1e20) {
+    cout << "beta=" << beta << ", xpom=" << xpom << ", Q2=" << Q2 << endl;
+    cout << "Q_s: " << Qs << ", r: " << r << ", z: " << z << ", R: " << R << ", theta: " << theta << endl;
+    cout << normalization*r*Phi_T*A << ", " << A << endl;;
+    cout << no_b_dipamp(R*Qs, xpom) << ", " << no_b_dipamp(rminusR*Qs, xpom) << ", -" << no_b_dipamp(r*Qs, xpom) << ", -" << no_b_dipamp(R*Qs, xpom)*no_b_dipamp(rminusR*Qs, xpom) << endl << endl;
+  }
   return normalization*r*Phi_T*A;
 }
 
@@ -292,11 +313,11 @@ struct qqg_LLbeta_parameters {
 double integration_function_qqg_LLbeta(double *k, size_t dim, void * params) {
   double r = k[0];
   double z = k[1];
-  double Rx = k[2];
-  double Ry = k[3];
+  double R = k[2];
+  double theta = k[3];
   struct qqg_LLbeta_parameters *par = (struct qqg_LLbeta_parameters *)params;
 
-  return Fqqg_LLbeta_integrand(par->xpom, par->Q2, par->beta, r, z, Rx, Ry);
+  return polar_Fqqg_LLbeta_integrand(par->xpom, par->Q2, par->beta, r, z, R, theta);
 }
 
 double xpomFqqg_LLbeta(double beta, double xpom, double Q2, double &result, double &error, double &fit) {
@@ -304,8 +325,9 @@ double xpomFqqg_LLbeta(double beta, double xpom, double Q2, double &result, doub
   const int dim = 4;
   double res, err;
 
-  double xl[dim] = {0, 0, -r_limit, -r_limit};
-  double xu[dim] = {r_limit, 1, r_limit, r_limit};
+  //const double range = 10000;
+  double xl[dim] = {0, 0, 0, 0};
+  double xu[dim] = {100, 1, 100, M_PI};
 
   struct qqg_LLbeta_parameters params = {1, 1, 1};
   params.beta = beta;
@@ -346,10 +368,10 @@ double xpomFqqg_LLbeta(double beta, double xpom, double Q2, double &result, doub
 
 int calc_total_xpomF_Tqqg_contribution(double beta, double xpom, double Q2, double &result, double &error, double fits[3]) {
   double LLQ2_result, LLQ2_error, LLQ2_fit;
-  //xpomFqqg_LLQ2(beta, xpom, Q2, LLQ2_result, LLQ2_error, LLQ2_fit);
+  xpomFqqg_LLQ2(beta, xpom, Q2, LLQ2_result, LLQ2_error, LLQ2_fit);
 
   double LLbeta_result, LLbeta_error, LLbeta_fit;
-  //xpomFqqg_LLbeta(beta, xpom, Q2, LLbeta_result, LLbeta_error, LLbeta_fit);
+  xpomFqqg_LLbeta(beta, xpom, Q2, LLbeta_result, LLbeta_error, LLbeta_fit);
 
   double LLQ2nul_result, LLQ2nul_error, LLQ2nul_fit;
   xpomFqqg_LLQ2(0, xpom, Q2, LLQ2nul_result, LLQ2nul_error, LLQ2nul_fit);
@@ -362,14 +384,88 @@ int calc_total_xpomF_Tqqg_contribution(double beta, double xpom, double Q2, doub
   return 0;
 }
 
-int main() {
+struct thread_par_struct
+{
+  double Q2;
+  double x;
+  double beta;
+  double &result;
+  double &error;
+  double &fit;
+  thread_par_struct(double a1, double a2, double a3, double &a4, double &a5, double &a6) : Q2(a1), x(a2), beta(a3), result(a4), error(a5), fit(a6) {}
+};
 
-  //gsl_set_error_handler_off();
-
-  double result, error;
-  double fits[3];
-  calc_total_xpomF_Tqqg_contribution(0.5, 3e-5, 1000, result, error, fits);
-  cout << "result: " << result << ", error: " << error << ", fits: " << fits[0] << ", " << fits[1] << ", " << fits[2] << endl << endl;
-
+int low_beta_thread_func(thread_par_struct par) {
+  xpomFqqg_LLbeta(par.beta, par.x, par.Q2, par.result, par.error, par.fit);
   return 0;
 }
+
+int main() {
+  /*
+  for(int i=0; i<1000; i++) {
+    double beta = 0.04;
+    double xpom = 0.00012/0.04;
+    double Q2 = 4.5;
+    double r = i/1000.0*10+1.0/1000;
+    cout << r << ", " << polar_Fqqg_LLbeta_integrand(beta, xpom, Q2, r, 0.5, r, 0.01) << endl;;
+  }
+  return 0;
+  */
+
+  gsl_set_error_handler_off();
+
+  double Result, Error, Fit;
+
+  xpomFqqg_LLbeta(0.04, 0.00012/0.04, 4.5, Result, Error, Fit);
+  cout << "result: " << Result << ", error: " << Error << ", fit: " << Fit << endl;
+  return 0;
+
+  vector<double> Q2_values, beta_values, x_values, x_pom_F2_values, delta_stat_values, delta_sys_values;
+
+  read_data_file("data/differential_HERA_data.dat", Q2_values, beta_values, x_values, x_pom_F2_values, delta_stat_values, delta_sys_values);
+
+  //string filename = "data/dipole_amplitude_with_IP_dependence_bk_p_diffraction.csv";
+  //load_p_dipole_amplitudes(table, filename);
+
+  thread low_beta_integration_threads[data_inclusion_count];
+  double result[data_inclusion_count], error[data_inclusion_count], fit[data_inclusion_count];
+
+  static auto t1 = chrono::high_resolution_clock::now();
+
+  for (int i=0; i<data_inclusion_count; i++) {
+
+    thread_par_struct low_beta_par(Q2_values[i+i_start], x_values[i+i_start], beta_values[i+i_start], result[i], error[i], fit[i]);
+    low_beta_integration_threads[i] = thread(low_beta_thread_func, low_beta_par);
+
+  }
+
+  for (int i=0; i<data_inclusion_count; i++) {
+    low_beta_integration_threads[i].join();
+  }
+
+  static auto t2 = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::seconds>(t2-t1);
+  cout << "Calculation finished in " << duration.count() << " seconds" << endl;
+
+  ofstream L_output_file("output/low_beta_corrections"+filename_end+".txt");
+  L_output_file << "Q2 (GeV);beta;x;sigma (mb);sigma error (mb);fit" << endl;
+
+  for (int i=0; i<data_inclusion_count; i++) {
+    ostringstream Q2;
+    Q2 << Q2_values[i+i_start];
+    ostringstream beta;
+    beta << beta_values[i+i_start];
+    ostringstream x;
+    x << x_values[i+i_start];
+    ostringstream result_stream;
+    result_stream << result[i];
+    ostringstream error_stream;
+    error_stream << error[i];
+    ostringstream fit_stream;
+    fit_stream << fit[i];
+    string line = Q2.str() + ";" + beta.str() + ";" + x.str() + ";" + result_stream.str() + ";" + error_stream.str() + ";" + fit_stream.str();
+    L_output_file << line << endl;
+  }
+  L_output_file.close();
+}
+
