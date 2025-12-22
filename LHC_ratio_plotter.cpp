39fd819351a1b33212c4c53320de620ebb081e65
JarnoVierros <vierros.jarno@gmail.com>
2025-12-22 09:06:52 +0200

    Add multiplotting

--- LHC_ratio_plotter.cpp
+++ LHC_ratio_plotter.cpp
@@ -16,39 +16,44 @@ using namespace std;
 
 int main() {
 
-  string numerator_filename, denominator_filename;
+  vector<string> numerator_filenames = {"bk_version", "bfkl_version"};
+  vector<string> denominator_filenames = {"bk_version", "bfkl_version"};
+
   TString title, outfile_name;
 
-  const string sigma_type = "Pb BK";
-  numerator_filename = "data/diff_LHC_T_sigma_W_c_bk_Pb_diffraction.txt";
-  denominator_filename = "data/J_LHC_T_inclusive_c_bk_Pb.txt";
-  title = "Diffractive "+sigma_type+" cross section divided by inclusive "+sigma_type+" cross section";
-  outfile_name = "figures/Pb_BK_mixed_dipamp_diff_inc_ratio.pdf";
-  
-  vector<double> numerator_W, numerator_sigma, numerator_sigma_error, denominator_W, denominator_sigma, denominator_sigma_error;
 
-  read_LHC_sigma_file(numerator_filename, numerator_W, numerator_sigma, numerator_sigma_error);
-  read_LHC_sigma_file(denominator_filename, denominator_W, denominator_sigma, denominator_sigma_error);
+  //const string sigma_type = "Pb BK";
+  //numerator_filename = "data/diff_LHC_T_sigma_W_c_bk_Pb_diffraction.txt";
+  //denominator_filename = "data/J_LHC_T_inclusive_c_bk_Pb.txt";
+  title = "Test";
+  outfile_name = "figures/Test.pdf";
 
+  TMultiGraph* ratios_graph = new TMultiGraph();
+  ratios_graph->SetTitle(title);
+  ratios_graph->GetXaxis()->SetTitle("W (GeV)");
   
-  double ratio[numerator_W.size()];
-  double W[numerator_W.size()];
-  for (long unsigned int j=0; j<numerator_W.size(); j++) {
-    ratio[j] = numerator_sigma[j]/denominator_sigma[j];
-    W[j] = numerator_W[j];
+  for (int i=0;i<numerator_filenames.size();i++) {
+    vector<double> numerator_W, numerator_sigma, numerator_sigma_error, denominator_W, denominator_sigma, denominator_sigma_error;
+    read_LHC_sigma_file(numerator_filenames[i], numerator_W, numerator_sigma, numerator_sigma_error);
+    read_LHC_sigma_file(denominator_filenames[i], denominator_W, denominator_sigma, denominator_sigma_error);
+    double ratio[numerator_W.size()];
+    double W[numerator_W.size()];
+    for (long unsigned int j=0; j<numerator_W.size(); j++) {
+      ratio[j] = numerator_sigma[j]/denominator_sigma[j];
+      W[j] = numerator_W[j];
+    }
+    TGraph* ratio_graph = new TGraph(numerator_W.size(), W, ratio);
+    ratio_graph->SetTitle("asd");
+    //ratio_graph->SetLineColor(2);
+    ratios_graph->Add(ratio_graph);
   }
 
-  TGraph* comparison_graph = new TGraph(numerator_W.size(), W, ratio);
-  comparison_graph->SetTitle(title);
-  comparison_graph->GetXaxis()->SetTitle("W (GeV)");
-  //comparison_graph->GetYaxis()->SetTitle("");
-
-  TCanvas* comparison_canvas = new TCanvas("comparison_canvas", "", 1000, 600);
-  comparison_graph->Draw("AC");
+  TCanvas* ratios_canvas = new TCanvas("ratios_canvas", "", 1000, 600);
+  ratios_graph->Draw("AC");
 
   gPad->SetLogx();
 
-  comparison_canvas->Print(outfile_name);
-  
+  ratios_canvas->Print(outfile_name);
+
   return 0;
 }
